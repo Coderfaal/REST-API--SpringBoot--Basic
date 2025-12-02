6870799437822d7602c9245e24033ba3a5b645c8
Coderfaal <fnargund4@gmail.com>
2025-12-02 12:16:25 +0000

    AOP Configurations

---
+++ src/main/java/com/spring/restfulapi/AOP/ResourceInterceptAspect.java
@@ -0,0 +1,37 @@
+package com.spring.restfulapi.AOP;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+//import com.spring.restfulapi.entity.Loan;
+
+
+
+@Aspect
+@Component
+@Slf4j         // lombok annotation for logging
+public class ResourceInterceptAspect {
+
+
+    @Pointcut("execution(  * com.spring.restfulapi.controller .*.*(..))")
+    public void loggingPointCut() { }
+
+
+    @Before("loggingPointCut()") // @Before means before advice
+    public void before(JoinPoint joinPoint) {
+
+        log.info("Before method invoked :: " + joinPoint.getSignature());
+
+    }
+
+    @After("loggingPointCut()") // @After means after advice
+    public void  after(JoinPoint joinPoint) {
+
+        log.info("After method invoked :: " + joinPoint.getSignature());
+
+    }
+}
